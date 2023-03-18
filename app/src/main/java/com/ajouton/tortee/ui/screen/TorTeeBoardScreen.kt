071329c530f80ad8c29ea307169876d796ccfefd
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2023-03-19 08:24:37 +0900

    UI Update

--- app/src/main/java/com/ajouton/tortee/ui/screen/TorTeeBoardScreen.kt
+++ app/src/main/java/com/ajouton/tortee/ui/screen/TorTeeBoardScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
@@ -118,7 +119,7 @@ fun BulletinBoardTopBar(
                 modifier = Modifier
                     .padding(10.dp)
                     .clip(shape = RoundedCornerShape(12.dp))
-                    .background(Color(R.color.search_bar))
+                    .background(color = Color(0xFFF0E9D2))
                     .padding(10.dp, 0.dp)
             ) {
                 TextField(
--- app/src/main/java/com/ajouton/tortee/ui/theme/Color.kt
+++ app/src/main/java/com/ajouton/tortee/ui/theme/Color.kt
@@ -4,6 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
-val Gray = Color(0xFF5e5e5e)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Gray = Color(0xFF181D31)
+val Secondery = Color(0xFFE6DDC4)
+val Teal200 = Color(0xFF678983)
