e663243ddd87719713e3efffc35cf2a21386b7bd
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2023-03-19 05:48:43 +0900

    Update Mentee Board

--- app/src/main/java/com/ajouton/tortee/ui/screen/TorTeeMyPageScreen.kt
+++ app/src/main/java/com/ajouton/tortee/ui/screen/TorTeeMyPageScreen.kt
@@ -1,9 +1,14 @@
 package com.ajouton.tortee.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -16,9 +21,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ajouton.tortee.R
 import com.ajouton.tortee.TorTeeViewModel
+import com.ajouton.tortee.model.Bulletin
+import com.ajouton.tortee.model.MenteeBulletin
 import com.ajouton.tortee.model.User
 import com.ajouton.tortee.ui.theme.TorTeeTheme
 
@@ -28,7 +37,12 @@ fun TorTeeMyPageScreen(
     viewModel: TorTeeViewModel,
 ) {
     var user by remember { mutableStateOf(User()) }
-
+    var dialogIndex by remember { mutableStateOf(0) }
+    when(dialogIndex) {
+        1 -> MyMentorDialog(onDismissRequest = { dialogIndex = 0 }, users = listOf())
+        2 -> MyMenteeDialog(onDismissRequest = { dialogIndex = 0 }, users = listOf())
+        3 -> MyHistoryDialog(onDismissRequest = { dialogIndex = 0 }, bulletins = listOf())
+    }
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -120,7 +134,7 @@ fun TorTeeMyPageScreen(
                 .fillMaxWidth()
         ) {
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { dialogIndex = 1 },
                 modifier = Modifier
                     .padding(10.dp)
                     .weight(1f)
@@ -131,7 +145,7 @@ fun TorTeeMyPageScreen(
                 )
             }
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { dialogIndex = 2 },
                 modifier = Modifier
                     .padding(10.dp)
                     .weight(1f)
@@ -142,7 +156,7 @@ fun TorTeeMyPageScreen(
                 )
             }
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { dialogIndex = 3 },
                 modifier = Modifier
                     .padding(10.dp)
                     .weight(1f)
@@ -154,6 +168,101 @@ fun TorTeeMyPageScreen(
             }
         }
     }
+}
+
+@Composable
+fun MyMentorGrid(
+    users: List<User>,
+    modifier: Modifier
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        modifier = Modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp)
+    ) {
+        items(users) { user ->
+            MyMentorCard(
+                user = user,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun MyMentorCard(
+    user: User,
+    modifier: Modifier,
+) {
+    androidx.compose.material.Card(
+        elevation = 4.dp,
+        modifier = modifier
+            .width(200.dp)
+            .height(230.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(24.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+        ) {
+            Image(
+                painter = painterResource(id = user.imageResId),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(10.dp)
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = user.rankResId),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = user.name,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+            TechnicListColumn(technics = user.technics)
+        }
+    }
+}
+@Composable
+fun MyMentorDialog(
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    users: List<User>
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties
+    ) {
+
+        MyMentorGrid(
+            users = users,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun MyMenteeDialog(
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    users: List<User>
+) {
+
+}
+
+fun MyHistoryDialog(
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    bulletins: List<Bulletin>
+) {
+
 }
 
 @Preview(showBackground = true)
