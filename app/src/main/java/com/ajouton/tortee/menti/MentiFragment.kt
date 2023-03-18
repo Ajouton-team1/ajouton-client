59032a2977f7c20977820211cfe7b90b66961891
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-03-18 21:33:58 +0900

    Update

--- app/src/main/java/com/ajouton/tortee/menti/MentiFragment.kt
+++ app/src/main/java/com/ajouton/tortee/menti/MentiFragment.kt
@@ -10,13 +10,13 @@ import com.ajouton.tortee.MainActivity
 import com.ajouton.tortee.databinding.FragmentMentiBinding
 
 
-class MentiFragment: Fragment() {
+class MentiFragment : Fragment() {
     private lateinit var binding: FragmentMentiBinding
-    val mentithumlist : ArrayList<Menti_thumnail> = arrayListOf(
-       Menti_thumnail("강하현","2023-3-17","객프 도와주세요"),
-        Menti_thumnail("강하현","2023-3-17","객프 도와주세요"),
-        Menti_thumnail("강하현","2023-3-17","객프 도와주세요"),
-        )
+    val mentithumlist: ArrayList<Menti_thumnail> = arrayListOf(
+        Menti_thumnail("강하현", "2023-3-17", "객프 도와주세요"),
+        Menti_thumnail("강하현", "2023-3-17", "객프 도와주세요"),
+        Menti_thumnail("강하현", "2023-3-17", "객프 도와주세요"),
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,8 +25,9 @@ class MentiFragment: Fragment() {
     ): View? {
         binding = FragmentMentiBinding.inflate(layoutInflater)
 
-        binding.mentiRv.layoutManager=LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false )
-        binding.mentiRv.adapter =  MentiAdapter(activity as MainActivity, mentithumlist)
+        binding.mentiRv.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        binding.mentiRv.adapter = MentiAdapter(activity as MainActivity, mentithumlist)
 
 
 
