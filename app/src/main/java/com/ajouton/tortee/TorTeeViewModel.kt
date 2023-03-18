8d6fbe1ad5c16127a16f5183b0893a30863dd70a
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2023-03-19 03:43:04 +0900

    Update

--- app/src/main/java/com/ajouton/tortee/TorTeeViewModel.kt
+++ app/src/main/java/com/ajouton/tortee/TorTeeViewModel.kt
@@ -2,26 +2,18 @@ package com.ajouton.tortee
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.ajouton.tortee.data.BoardDataProvider
 import com.ajouton.tortee.model.Bulletin
 import com.ajouton.tortee.data.ViewType
-import com.ajouton.tortee.model.User
 import com.ajouton.tortee.network.TorTeeApiService
-import com.ajouton.tortee.network.UserSignUpRequest
+import com.ajouton.tortee.network.UserSignInRequest
+import com.ajouton.tortee.network.UserSignInResponse
 import com.ajouton.tortee.ui.state.TorteeUIState
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -29,7 +21,7 @@ import java.io.IOException
 
 class TorTeeViewModel() : ViewModel() {
 
-    private val BASE_URL = "http://144.24.75.46:3000/auth/login"
+    private val BASE_URL = "http://144.24.75.46:3000/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
@@ -41,17 +33,26 @@ class TorTeeViewModel() : ViewModel() {
     }
 
     private val _uiState = MutableStateFlow(TorteeUIState())
+    private val _signUpPageVisibility = MutableStateFlow(false)
+    private val _userIdInput = MutableStateFlow("")
+    private val _userPasswordInput = MutableStateFlow("")
     private val _isBulletinContentShowing = MutableStateFlow(false)
     private val _isBulletinWriterShowing = MutableStateFlow(false)
     private val _selectedBulletin = MutableStateFlow(Bulletin("", "", ""))
-    private val _userSignUpRequest = MutableStateFlow(UserSignUpRequest("", "", "", ""))
+    private val _userSignUpRequest = MutableStateFlow(UserSignInRequest("", ""))
+    private val _isSignedIn = MutableStateFlow(false)
+    private val _userSignInResponse = MutableStateFlow(UserSignInResponse(false, 0))
 
     val uiState: StateFlow<TorteeUIState> = _uiState
+    val signUpPageVisibility: StateFlow<Boolean> = _signUpPageVisibility
+    val userIdInput: StateFlow<String> = _userIdInput
+    val userPasswordInput: StateFlow<String> = _userPasswordInput
     val isBulletinContentShowing: StateFlow<Boolean> = _isBulletinContentShowing
     val isBulletinWriterShowing: StateFlow<Boolean> = _isBulletinWriterShowing
     val selectedBulletin: StateFlow<Bulletin> = _selectedBulletin
-    val userSignUpRequest: StateFlow<UserSignUpRequest> = _userSignUpRequest
-    var isSignUpOK: Boolean = true
+    val userSignUpRequest: StateFlow<UserSignInRequest> = _userSignUpRequest
+    val isSignedIn: StateFlow<Boolean> = _isSignedIn
+    val userSignInResponse: StateFlow<UserSignInResponse> = _userSignInResponse
 
     fun updateCurrentView(viewType: ViewType) {
         _uiState.update {
@@ -59,6 +60,18 @@ class TorTeeViewModel() : ViewModel() {
                 currentViewType = viewType
             )
         }
+    }
+
+    fun updateUserIdInput(input: String) {
+        _userIdInput.update { input }
+    }
+
+    fun updateUserPasswordInput(input: String) {
+        _userPasswordInput.update { input }
+    }
+
+    fun setSignUpPageVisibility(visibility: Boolean) {
+        _signUpPageVisibility.update { visibility }
     }
 
     fun setBulletinWriterVisibility() {
@@ -80,18 +93,26 @@ class TorTeeViewModel() : ViewModel() {
 
     }
 
-    fun signUp(user: UserSignUpRequest) {
-        val userSignUpRequest = UserSignUpRequest("email@gmail.com", "name", "nickname", "password")
+    fun signIn(user: UserSignInRequest) {
+        val userSignInRequest = UserSignInRequest(userIdInput.value, userPasswordInput.value)
         viewModelScope.launch {
-            isSignUpOK = try {
-                Log.e("signup",isSignUpOK.toString())
-                retrofitService.signUp("token", userSignUpRequest)
-            } catch(e: IOException) {
-                Log.e("signup","IOException")
-                false
-            } catch(e: HttpException) {
-                Log.e("signup","HttpException")
-                false
+            _userSignInResponse.update {
+                try {
+                    Log.e("signup", "Success")
+                    retrofitService.signIn(userSignInRequest)
+                } catch(e: IOException) {
+                    e.printStackTrace()
+                    Log.e("signup","IOException")
+                    UserSignInResponse(false, 0)
+                } catch(e: HttpException) {
+                    Log.e("signup","HttpException")
+                    UserSignInResponse(false, 0)
+                }
+            }
+            _isSignedIn.update {
+                Log.e("", userSignInResponse.value.result.toString())
+                Log.e("", userSignInResponse.value.id.toString())
+                userSignInResponse.value.result
             }
         }
     }
--- app/src/main/java/com/ajouton/tortee/ui/screen/SignUpScreen.kt
+++ app/src/main/java/com/ajouton/tortee/ui/screen/SignUpScreen.kt
@@ -1,19 +1,11 @@
 package com.ajouton.tortee.ui.screen
 
-import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import com.ajouton.tortee.TorTeeViewModel
-import com.ajouton.tortee.model.User
-import com.ajouton.tortee.network.UserSignUpRequest
 
 @Composable
 fun SignUpScreen(
     viewModel: TorTeeViewModel
 ) {
-    val userSignUpRequest = UserSignUpRequest("email", "name", "nickname", "password")
-    Button(onClick = {
-        viewModel.signUp(userSignUpRequest)
-    }) {
 
-    }
 }
