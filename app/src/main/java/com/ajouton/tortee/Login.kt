6000e2834c4886f5bad6d890e191356b69a72da6
xotourlilf3 <127809073+xotourlilf3@users.noreply.github.com>
2023-03-18 21:55:58 +0900

    Update

--- app/src/main/java/com/ajouton/tortee/Login.kt
+++ app/src/main/java/com/ajouton/tortee/Login.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.Volley
